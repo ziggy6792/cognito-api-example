45429cc26f2489ecccab042f9a04b6efe5d020d6
Simon <simonaverhoeven@gmail.com>
2020-12-08 20:09:31 +0800

    fixed cdk versioning

--- cognito-api-example/lib/cognito-api-example-stack.ts
+++ cognito-api-example/lib/cognito-api-example-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { CognitoToApiGatewayToLambda } from '@aws-solutions-constructs/aws-cognito-apigateway-lambda';
 
 export class CognitoApiExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,12 +10,21 @@ export class CognitoApiExampleStack extends cdk.Stack {
       return `${id}-${constructId}`;
     };
 
-    const handler = new lambda.Function(this, generateConstructId('TodoHandler'), {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'todoHandler.handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
-      environment: {},
+    // const handler = new lambda.Function(this, generateConstructId('TodoHandler'), {
+    //   code: lambda.Code.fromAsset('lambda'),
+    //   handler: 'todoHandler.handler',
+    //   runtime: lambda.Runtime.NODEJS_12_X,
+    //   environment: {},
+    // });
+
+    new CognitoToApiGatewayToLambda(this, generateConstructId('TodoHandler'), {
+      lambdaFunctionProps: {
+        code: lambda.Code.fromAsset('lambda'),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'index.handler',
+      },
     });
+
     // The code that defines your stack goes here
   }
 }
