c22d54b9da182e26b62fbfb59aec9dfbe4cc06df
Simon <simonaverhoeven@gmail.com>
2020-12-08 23:58:07 +0800

    working example

--- cognito-api-example/lambda/index.ts
+++ cognito-api-example/lambda/index.ts
@@ -4,19 +4,24 @@ import { v4 as uuid } from 'uuid';
 const tableName = process.env.TABLE_NAME || '';
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-const createResponse = (body: string | AWS.DynamoDB.DocumentClient.ItemList, statusCode = 200) => {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,DELETE',
-    },
-    statusCode,
-    body: JSON.stringify(body, null, 2),
-  };
-};
+// const createResponse = (body: string | AWS.DynamoDB.DocumentClient.ItemList, statusCode = 200) => {
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//     },
+//     body: JSON.stringify({ message: 'Authenticated call!' }),
+//   };
+// };
 
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
   const { httpMethod, body: requestBody } = event;
 
-  return createResponse('OKAY');
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({ message: 'Authenticated call!' }),
+  };
 };
--- cognito-api-example/lib/cognito-api-example-stack.ts
+++ cognito-api-example/lib/cognito-api-example-stack.ts
@@ -18,19 +18,19 @@ export class CognitoApiExampleStack extends cdk.Stack {
         runtime: lambda.Runtime.NODEJS_12_X,
         handler: 'index.handler',
       },
-      // apiGatewayProps: {
-      //   proxy: false,
-      // },
+      apiGatewayProps: {
+        proxy: false,
+      },
     });
 
     // For first try I am just trying to bit the base url so I commented out this stuff (though I did try with it uncommented too)
 
-    // const resource = construct.apiGateway.root.addResource('hello');
-    // resource.addMethod('GET');
-    // resource.addMethod('POST');
+    const resource = construct.apiGateway.root.addResource('hello');
+    resource.addMethod('GET');
+    resource.addMethod('POST');
 
     // Mandatory to call this method to Apply the Cognito Authorizers on all API methods
-    // construct.addAuthorizers();
+    construct.addAuthorizers();
 
     // The code that defines your stack goes here
   }
