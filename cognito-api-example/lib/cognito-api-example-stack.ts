a74a7450a67a0a9f3eeccda2d25f3e5e05a975f9
Simon <simonaverhoeven@gmail.com>
2020-12-11 17:24:20 +0800

    set domain name

--- cognito-api-example/lib/cognito-api-example-stack.ts
+++ cognito-api-example/lib/cognito-api-example-stack.ts
@@ -3,7 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import MultiAuthApiGatewayLambda from '../constructs/multi-auth-apigateway-lambda';
 import * as iam from '@aws-cdk/aws-iam';
 import * as cognito from '@aws-cdk/aws-cognito';
-
+import * as defaults from '@aws-solutions-constructs/core';
 export class CognitoApiExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -60,6 +60,36 @@ export class CognitoApiExampleStack extends cdk.Stack {
         accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
       },
     });
+
+    // const cognitoDomain = construct.userPool.addDomain(generateConstructId('api'),{})
+
+    // const userPoolDomain = new cognito.UserPoolDomain(this, 'UserPoolDomain', {
+    //   userPool: construct.userPool,
+    //   customDomain: {
+    //     domainName: construct.apiGateway.restApiId,
+    //     cognitoDomain,
+    //     // certificate,
+    //   },
+    // });
+
+    // const client = construct.userPool.addClient('stackoverflow-userpool-localhost-client', {
+    //   userPoolClientName: 'stackoverflow-localhost-client',
+    //   oAuth: {
+    //     flows: { authorizationCodeGrant: true, implicitCodeGrant: true },
+    //     scopes: [cognito.OAuthScope.OPENID, cognito.OAuthScope.EMAIL, cognito.OAuthScope.PHONE, cognito.OAuthScope.COGNITO_ADMIN],
+    //     callbackUrls: ['http://localhost:3000/'],
+    //     logoutUrls: ['http://localhost:3000/'],
+    //   },
+    //   // supportedIdentityProviders: [cognito.UserPoolClientIdentityProvider.AMAZON, cognito.UserPoolClientIdentityProvider.COGNITO],
+    // });
+
+    construct.userPool.addDomain('CognitoDomain', {
+      cognitoDomain: {
+        domainPrefix: construct.apiGateway.restApiId,
+      },
+    });
+
+    // defaults.printWarning(construct.apiGateway.restApiId);
 
     const externalProxy = construct.externalResource.addProxy();
     externalProxy.addMethod('GET');
